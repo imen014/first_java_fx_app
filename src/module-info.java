14eb0d0c8ca8664c2252039119117d08d3a699fd
imen rezgui <rezgui.imen1093@gmail.com>
2024-08-19 23:34:25 +0100

    first commit

--- src/module-info.java
+++ src/module-info.java
@@ -8,6 +8,8 @@
 module emp_management {
     requires javafx.controls;
     requires javafx.fxml;
+	requires java.sql;
+	requires javafx.base;
 
     opens emp_management to javafx.fxml;
     exports emp_management;
